ffe06016804c94344f2d45c952e7e4c220cf4118
Arnav Gupta <arnavgupta.svt@gmail.com>
2025-07-03 12:30:35 +0530

    Update Home.js

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,1 +1,22 @@
 
+import React from 'react';
+import Hero from './Hero.js';
+
+function Home() {
+  return (
+    
+          <>
+      
+          <div>
+            <Hero />
+          
+        
+          </div>
+          </>
+        
+      
+  );
+}
+
+export default Home;
+     
